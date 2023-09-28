98b0120f504b53ea9c6825e128215c48e497a3b4
mr.valerio <valera.osadchuk@gmail.com>
2023-09-28 14:34:56 +0300

    REDUX:ADD: store with basic logic to search movies

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,3 +1,8 @@
+import { useSearchMovieQuery } from "../store/tmdb/tmdb.api";
+
 export default function HomePage() {
+  const { isLoading, isError, data } = useSearchMovieQuery("rambo");
+  console.log("data", data);
+
   return <div>HomePage</div>;
 }
---
+++ src/store/index.ts
@@ -0,0 +1,14 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { tmdbApi } from "./tmdb/tmdb.api";
+
+// created 'store' should be 'provided' for whole app
+// see 'main.tsx' line 10 - <Provider store={store}>...
+export const store = configureStore({
+  reducer: {
+    // this key will have name 'tmdb/api'
+    [tmdbApi.reducerPath]: tmdbApi.reducer,
+  },
+  // inject default middleware to avoid warnings in browser's console
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(tmdbApi.middleware),
+});
