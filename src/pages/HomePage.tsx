eefd1e45d2fd12144fa0f200b41a641321e97d87
mr.valerio <valera.osadchuk@gmail.com>
2023-10-02 22:21:12 +0300

    REACT:ADD: search for collection and get collectoin data

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,104 @@
-import { useSearchMovieQuery } from "../store/tmdb/tmdb.api";
+const DEBOUNCE_DELAY = 500;
+const MIN_SEARCH_TERM_LENGTH = 4;
+
+import { useEffect, useState } from "react";
+import {
+  useSearchCollectionQuery,
+  useLazyGetCollectionQuery,
+} from "../store/tmdb/tmdb.api";
+import { useDebounce } from "../hooks/debounce";
+
+const Error = () => (
+  <span className="text-red-400">Something went wrong...</span>
+);
 
 export default function HomePage() {
-  const { isLoading, isError, data } = useSearchMovieQuery("rambo");
-  console.log("data", data);
+  const [term, setTerm] = useState("");
+  // show/hide dropdown if there's a data and debounced > 3 symbols
+  const [dropdown, setDropdown] = useState(false);
+  const debounced = useDebounce(term, DEBOUNCE_DELAY);
 
-  return <div>HomePage</div>;
+  const {
+    isLoading: isCollectionsSearchLoading,
+    isError: isCollectionsSearchError,
+    data: collectionsSearchData,
+  } = useSearchCollectionQuery(debounced, {
+    skip: debounced.length < MIN_SEARCH_TERM_LENGTH,
+  });
+
+  const [
+    getCollection,
+    {
+      isLoading: isCollectionLoading,
+      isError: isCollectionError,
+      data: collectionData,
+    },
+  ] = useLazyGetCollectionQuery();
+
+  const handleCollectionsItemClick = (id: number) => {
+    getCollection(id);
+    setDropdown(false);
+  };
+
+  useEffect(
+    () =>
+      setDropdown(debounced.length >= MIN_SEARCH_TERM_LENGTH ? true : false),
+    [collectionsSearchData, debounced]
+  );
+
+  return (
+    <>
+      <div className="container flex justify-between items-center mx-auto p-4">
+        <div className="w-3/5 mx-auto">
+          <input
+            type="text"
+            className="form-input px-4 py-3 rounded-full w-full"
+            placeholder="search for a movie"
+            value={term}
+            onChange={(e) => setTerm(e.target.value)}
+          />
+
+          <div className="max-h-[70vh] overflow-y-auto rounded-md shadow bg-slate-50">
+            {isCollectionsSearchError ? (
+              <Error />
+            ) : (
+              dropdown && (
+                <ul className="list-none">
+                  {collectionsSearchData &&
+                  collectionsSearchData.length === 0 ? (
+                    <li className="text-slate-400 py-2 px-4">
+                      nothing found...
+                    </li>
+                  ) : isCollectionsSearchLoading ? (
+                    <li className="text-sky-400 py-2 px-4">fetching data...</li>
+                  ) : (
+                    collectionsSearchData?.map((collection) => (
+                      <li
+                        className="p-4 cursor-pointer hover:bg-slate-100 hover:text-sky-400"
+                        key={collection.id}
+                        onClick={() =>
+                          handleCollectionsItemClick(collection.id)
+                        }
+                      >
+                        {collection.name}
+                      </li>
+                    ))
+                  )}
+                </ul>
+              )
+            )}
+          </div>
+          <pre className="w-[100%]">
+            {isCollectionError ? (
+              <Error />
+            ) : isCollectionLoading ? (
+              "loading..."
+            ) : (
+              JSON.stringify(collectionData, null, 2)
+            )}
+          </pre>
+        </div>
+      </div>
+    </>
+  );
 }
