4185e3c5d241aa2b47c27ea9592ab3f012385fc2
mr.valerio <valera.osadchuk@gmail.com>
2023-10-02 22:41:46 +0300

    PROJ:ADD: dark theme

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -2,12 +2,16 @@ import { Link } from "react-router-dom";
 
 export default function Navigation() {
   return (
-    <nav className="shadow-md bg-sky-600 text-white">
+    <nav className="shadow-md bg-sky-600 text-sky-100 dark:bg-sky-800 dark:text-sky-200">
       <div className="container flex justify-between items-center h-12 mx-auto p-4">
-        <h3>TMDB Search</h3>
+        <h3 className="bold">TMDB Search</h3>
         <div className="flex items-center gap-6">
-          <Link to="/">Home</Link>
-          <Link to="/favorites">Favorites</Link>
+          <Link className="hover:text-sky-400" to="/">
+            Home
+          </Link>
+          <Link className="hover:text-sky-400" to="/favorites">
+            Favorites
+          </Link>
         </div>
       </div>
     </nav>
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -52,7 +52,7 @@ export default function HomePage() {
         <div className="w-3/5 mx-auto">
           <input
             type="text"
-            className="form-input px-4 py-3 rounded-full w-full"
+            className="form-input px-4 py-3 rounded-full w-full dark:bg-slate-900 dark:text-slate-50"
             placeholder="search for a movie"
             value={term}
             onChange={(e) => setTerm(e.target.value)}
@@ -63,7 +63,7 @@ export default function HomePage() {
               <Error />
             ) : (
               dropdown && (
-                <ul className="list-none">
+                <ul className="list-none dark:bg-slate-900 dark:text-slate-300">
                   {collectionsSearchData &&
                   collectionsSearchData.length === 0 ? (
                     <li className="text-slate-400 py-2 px-4">
@@ -74,7 +74,7 @@ export default function HomePage() {
                   ) : (
                     collectionsSearchData?.map((collection) => (
                       <li
-                        className="p-4 cursor-pointer hover:bg-slate-100 hover:text-sky-400"
+                        className="p-4 cursor-pointer hover:bg-slate-100 hover:text-sky-400 dark:hover:bg-slate-700 dark:hover:text-sky-400"
                         key={collection.id}
                         onClick={() =>
                           handleCollectionsItemClick(collection.id)
@@ -88,7 +88,7 @@ export default function HomePage() {
               )
             )}
           </div>
-          <pre className="w-[100%]">
+          <pre className="w-[100%] dark:text-sky-300">
             {isCollectionError ? (
               <Error />
             ) : isCollectionLoading ? (
