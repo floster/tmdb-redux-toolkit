1937c3fd94432f6fa2bdb9bcf2a02810bf30114c
mr.valerio <valera.osadchuk@gmail.com>
2023-10-02 22:20:33 +0300

    REACT:FIX: Navigation visual improvements

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -2,12 +2,13 @@ import { Link } from "react-router-dom";
 
 export default function Navigation() {
   return (
-    <nav className="flex justify-between items-center h-12 px-5 shadow-md bg-sky-600 text-white">
-      <h3>TMDB Search</h3>
-
-      <div className="flex items-center gap-3">
-        <Link to="/">Home</Link>
-        <Link to="/favorites">Favorites</Link>
+    <nav className="shadow-md bg-sky-600 text-white">
+      <div className="container flex justify-between items-center h-12 mx-auto p-4">
+        <h3>TMDB Search</h3>
+        <div className="flex items-center gap-6">
+          <Link to="/">Home</Link>
+          <Link to="/favorites">Favorites</Link>
+        </div>
       </div>
     </nav>
   );
